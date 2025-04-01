9fa915f1517ae2b7d6620aa69ccbdf94b038d0a9
Gabriel de Souza <gabrielsouza080802@gmail.com>
2025-04-01 14:39:01 -0300

    Terceiro Commit

--- divisores/src/main/java/divisores/Main.java
+++ divisores/src/main/java/divisores/Main.java
@@ -10,6 +10,7 @@ public class Main {
         Scanner input = new Scanner(System.in);
         System.out.println("Digite um número: ");
         num = input.nextInt();
+        input.close();
 
         ArrayList<Integer> lista = calcularDivisores(num);
         System.out.println("Divisores de " + num + ":");
