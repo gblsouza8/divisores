a20c0ca3404abe6c7fa9f1b6891ba9c1d8c9199f
Gabriel de Souza <gabrielsouza080802@gmail.com>
2025-04-01 14:42:16 -0300

    Quarto Commit, comentários adicionados

--- divisores/src/main/java/divisores/Main.java
+++ divisores/src/main/java/divisores/Main.java
@@ -2,18 +2,27 @@ package divisores;
 
 import java.util.ArrayList;
 import java.util.Scanner;
-
+// programa que calcula e exibe os divisores de um número
 public class Main {
     public static void main(String[] args) {
+        // inicializa a variavel em que será armazenada o numero do usuário
         int num;
 
+        // inicializa o scanner e armazena o numero do usuario na variavel num
         Scanner input = new Scanner(System.in);
         System.out.println("Digite um número: ");
         num = input.nextInt();
+
+        // fecha o scanner
         input.close();
 
+        // cria a arraylist lista contendo os numeros que obtivemos no metódo
         ArrayList<Integer> lista = calcularDivisores(num);
+
+        // exibe uma mensagem
         System.out.println("Divisores de " + num + ":");
+
+        // laço percorre toda a lista e existe os numeros
         for (Integer lista1 : lista) {
             System.out.println(lista1);
         }
@@ -21,17 +30,22 @@ public class Main {
 
     }
 
+    // metódo que faz o cálculo
     public static ArrayList<Integer> calcularDivisores(int num)
     {
+        // inicializa a arraylist em que será armazenado os divisores
         ArrayList<Integer> lista = new ArrayList<Integer>();
         for (int i = 1; i <= num; i++)
         {
+            // verifica se o resto da divisão entre o numero e o atual i é 0
             if (num%i == 0)
             {
+                // se for, adiciona o numero na lista
                 lista.add(i);
             }
         }
 
+        // retorna a lista
         return lista;
     }
 }
